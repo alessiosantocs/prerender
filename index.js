370997dbb315e6141f855b4df6a39baabb533d23
Alessio Santo <alessio.santocs@gmail.com>
2014-01-08 19:04:49 +0100

    in memory cache activated

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ prerender = require('./lib/prerender');
 prerender.use(require('./lib/plugins/blacklist'));
 // prerender.use(require('./lib/plugins/logger'));
 prerender.use(require('./lib/plugins/remove-script-tags'));
-// prerender.use(require('./lib/plugins/in-memory-html-cache'));
-prerender.use(require('./lib/plugins/s3-html-cache'));
+prerender.use(require('./lib/plugins/in-memory-html-cache'));
+// prerender.use(require('./lib/plugins/s3-html-cache'));
 prerender.use(require('./lib/plugins/http-headers'));
 
 prerender.createServer();
